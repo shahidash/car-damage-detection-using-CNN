f189e89e739ed473588f88ff1a358c4e027a5e99
shahidash <shahid@trialx.com>
2020-03-02 15:04:39 +0530

    change for ddte

--- custom_1.py
+++ custom_1.py
@@ -38,7 +38,7 @@ from mrcnn.visualize import display_instances
 import matplotlib.pyplot as plt
 
 # Root directory of the project
-ROOT_DIR = 'C:/Users/Sourish/Mask_RCNN/'
+ROOT_DIR = '/Users/shahid/projects/custom_directory'
 
 # Import Mask RCNN
 sys.path.append(ROOT_DIR)  # To find local version of the library
@@ -62,14 +62,14 @@ class CustomConfig(Config):
     Derives from the base Config class and overrides some values.
     """
     # Give the configuration a recognizable name
-    NAME = "scratch"
+    NAME = "damage"
 
     # We use a GPU with 6GB memory, which can fit only one image.
     # Adjust down if you use a smaller GPU.
     IMAGES_PER_GPU = 1
 
     # Number of classes (including background)
-    NUM_CLASSES = 1 + 1  # Car Background + scratch
+    NUM_CLASSES = 1 + 1  # Car Background + damage
 
     # Number of training steps per epoch
     STEPS_PER_EPOCH = 100
@@ -90,7 +90,7 @@ class CustomDataset(utils.Dataset):
         subset: Subset to load: train or val
         """
         # Add classes. We have only one class to add.
-        self.add_class("scratch", 1, "scratch")
+        self.add_class("damage", 1, "damage")
 
         # Train or validation dataset?
         assert subset in ["train", "val"]
@@ -111,7 +111,8 @@ class CustomDataset(utils.Dataset):
         #   'size': 100202
         # }
         # We mostly care about the x and y coordinates of each region
-        annotations1 = json.load(open(os.path.join(dataset_dir + '/' + "via_region_data.json"),'r',encoding="utf8",errors='ignore'))
+        print ("ds:", dataset_dir)
+        annotations1 = json.load(open(os.path.join(dataset_dir + '/' + "via_export_json.json"),'r',encoding="utf8",errors='ignore'))
         # print(annotations1)
         annotations = list(annotations1.values())  # don't need the dict keys
 
@@ -121,21 +122,23 @@ class CustomDataset(utils.Dataset):
 
         # Add images
         for a in annotations:
-            # print(a)
+            print(a)
             # Get the x, y coordinaets of points of the polygons that make up
             # the outline of each object instance. There are stores in the
             # shape_attributes (see json format above)
-            polygons = [r['shape_attributes'] for r in a['regions'].values()]
+            polygons = [r['shape_attributes'] for r in a['regions']]
+            print ("polygons"*10,polygons)
 
             # load_mask() needs the image size to convert polygons to masks.
             # Unfortunately, VIA doesn't include it in JSON, so we must read
             # the image. This is only managable since the dataset is tiny.
             image_path = os.path.join(dataset_dir, a['filename'])
+            print ("imge_path:" , image_path)
             image = skimage.io.imread(image_path)
             height, width = image.shape[:2]
 
             self.add_image(
-                "scratch",  ## for a single class just add the name here
+                "damage",  ## for a single class just add the name here
                 image_id=a['filename'],  # use file name as a unique image id
                 path=image_path,
                 width=width, height=height,
@@ -150,7 +153,7 @@ class CustomDataset(utils.Dataset):
         """
         # If not a balloon dataset image, delegate to parent class.
         image_info = self.image_info[image_id]
-        if image_info["source"] != "scratch":
+        if image_info["source"] != "damage":
             return super(self.__class__, self).load_mask(image_id)
 
         # Convert polygons to a bitmap mask of shape
@@ -160,8 +163,15 @@ class CustomDataset(utils.Dataset):
                         dtype=np.uint8)
         for i, p in enumerate(info["polygons"]):
             # Get indexes of pixels inside the polygon and set them to 1
-            rr, cc = skimage.draw.polygon(p['all_points_y'], p['all_points_x'])
-            mask[rr, cc, i] = 1
+            print ("plygon", p,i)
+            if p["name"] == "circle":
+                continue
+            elif p["name"] == "rect":
+                print ("rectangel"*10)
+                # rr, cc = skimage.draw.rectangle((p['x'],p['x']), extent=(p['y'],p['y']), shape=(info["width"], info["height"]))
+            else:
+                rr, cc = skimage.draw.polygon(p['all_points_y'], p['all_points_x'])
+                mask[rr, cc, i] = 1
 
         # Return mask, and array of class IDs of each instance. Since we have
         # one class ID only, we return an array of 1s
@@ -170,7 +180,7 @@ class CustomDataset(utils.Dataset):
     def image_reference(self, image_id):
         """Return the path of the image."""
         info = self.image_info[image_id]
-        if info["source"] == "scratch":
+        if info["source"] == "damage":
             return info["path"]
         else:
             super(self.__class__, self).image_reference(image_id)
